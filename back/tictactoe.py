81e35564a6456124b9389e07cbbe308a8062e293
Heritiana Daniel Andriasolofo <aheritianad@gmail.com>
2022-09-26 16:50:24 +0000

    Add dictionary actions

--- back/tictactoe.py
+++ back/tictactoe.py
@@ -15,6 +15,8 @@ class TicTacToe:
         self.color = [0]*8
         self.end = False
         self.winner = None
+        self.actions = {0: (0, 0), 1: (0, 1), 2: (0, 2), 3: (1, 0), 4: (
+            1, 1), 5: (1, 2), 6: (2, 0), 7: (2, 1), 8: (2, 2)}
 
     def whose_turn(self):
         return 0 if self.val == 1 else 1
